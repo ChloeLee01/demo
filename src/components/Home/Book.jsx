ae2a87e48b6bbb77efa1754d40cd787452129507
ChloeLee01 <youngsunyi01@gmail.com>
2024-11-18 12:21:39 +0900

    book info

--- src/components/Home/Book.jsx
+++ src/components/Home/Book.jsx
@@ -1,27 +1,37 @@
 import React from "react";
 import tablet from "./../../images/tabletScreen.jpg";
+import BookNav from "./BookNav";
+import InsideBook from "./InsideBook";
 const Book = () => {
   return (
     <div className="wrapper">
       <div className="container">
         <div className="first-section flex items-center justify-between">
-          <div className="img-box w-1/2">
-            <img src={tablet} alt="" />
+          <div className="img-box w-5/8 flex translate-y-14">
+            <img src={tablet} alt="tablet" />
           </div>
-          <div className="des-box w-1/2">
-            <p>Modern & Creative</p>
-            <p>About The Book</p>
-            <p className="book-text">
-              Credit goes to <span className="text-red-600">FreePik</span> for
-              images used in this ebook landing page template. You may browse
-              FreePik to download more free images for your website.
-            </p>
-            <p className="book-text">
-              TemplateMo is one of the best website to download free CSS
-              templates for any purpose. This is an ebook landing page template
-              using Bootstrap 5 CSS layout
-            </p>
+          <div className="des-box w-3/8 flex items-baseline pl-5">
+            <div>
+              <p className="font-semibold text-xl text-amber-300">Modern & Creative</p>
+              <p className="font-semibold text-[3rem]">About The Book</p>
+              <p className="book-text leading-7">
+                Credit goes to <span className="text-red-600">FreePik</span> for
+                images used in this ebook landing page template. You may browse
+                FreePik to download more free images for your website.
+              </p>
+              <p className="book-text mt-3">
+                TemplateMo is one of the best website to download free CSS
+                templates for any purpose. This is an ebook landing page template
+                using Bootstrap 5 CSS layout
+              </p>
+            </div>
           </div>
+        </div>
+      </div>
+      <div className="container">
+        <div className="book-wrapper flex">
+          <BookNav/>
+          <InsideBook/>
         </div>
       </div>
     </div>
---
+++ src/components/Home/InsideBook.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const InsideBook = () => {
+  return (
+    <div className='w-2/3'>InsideBook</div>
+  )
+}
+
+export default InsideBook
